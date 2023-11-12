7aa80d2d00dfcd98b2223942c0fa76bd2bb64052
sanseyooyea <1187586838@qq.com>
2023-11-12 16:30:13 +0800

    get properties as stream

--- src/main/java/work/microhand/model/config/DatabaseConfig.java
+++ src/main/java/work/microhand/model/config/DatabaseConfig.java
@@ -1,23 +1,20 @@
 package work.microhand.model.config;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
 import java.util.Properties;
 
 public class DatabaseConfig {
 
-    private static Properties properties;
+    private static final Properties PROPERTIES = new Properties();
 
     public static Properties getProperties() {
-        return properties;
+        return PROPERTIES;
     }
 
     public static void init() {
         try (InputStream is = DatabaseConfig.class.getResourceAsStream("/database.properties")) {
-            properties.load(is);
+            PROPERTIES.load(is);
         } catch (IOException e) {
             System.out.println("Failed to load config file: " + e.getMessage());
         }
